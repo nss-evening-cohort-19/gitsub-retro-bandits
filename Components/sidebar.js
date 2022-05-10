8ccaad48d487815b36c3279ef17b76a96ad4bad2
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-05-09 21:08:01 -0500

    tried merging with main again

--- Components/sidebar.js
+++ Components/sidebar.js
@@ -1,22 +1,23 @@
 import renderToDom from "../Utils/renderToDom.js"
+import users from "../Data/data.js"
 
-domString = "";
+let domString = "";
 
 const sidebarBody = () => {
   domString = `<div class="card">
   <img src="..." class="card-img-top" alt="user's profile image">
   <div class="card-body">
-    <h5 id="userName" class="card-title">${array.key}</h5>
-    <h4 id="preferredName" class="card-title">${array.key}</h4>
-    <p id="userBio" class="card-text">${array.key}</p>
+    <h5 id="name" class="card-title">${users.name}</h5>
+    <h4 id="userName" class="card-title">${users.username}</h4>
+    <p id="userBio" class="card-text">Bio here</p>
     <a href="#followBtn" class="btn btn-primary">Follow</a>
     <a href="#sponsorBtn" class="btn btn-primary">Sponsor</a>
     <a href="#ellipsisBtn" class="btn btn-primary">...</a>
-    <p id= "userStats" class="card-text"><span class="iconify" data-icon="octicon:people-16" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key} followers • ${array.key} following • <span class="iconify" data-icon="cil:star" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key}</p>
-    <p id="userLocation" class="card-text"> <span class="iconify" data-icon="ci:location-outline" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key}</p>
-    <p id="userEmail" class="card-text"><span class="iconify" data-icon="carbon:email" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key}</p>
-    <p id="userWebsite" class="card-text"><span class="iconify" data-icon="akar-icons:link-chain" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key}</p>
-    <p id="userTwitter" class="card-text"><span class="iconify" data-icon="icon-park-solid:twitter" style="color: lightgray;" data-width="1" data-height="1"></span> ${array.key}</p>
+    <p id= "userStats" class="card-text"><span class="iconify" data-icon="octicon:people-16" style="color: lightgray;" data-width="1" data-height="1"></span> # followers • # following • <span class="iconify" data-icon="cil:star" style="color: lightgray;" data-width="1" data-height="1"></span> favorites </p>
+    <p id="userLocation" class="card-text"> <span class="iconify" data-icon="ci:location-outline" style="color: lightgray;" data-width="1" data-height="1"></span> ${users.location}</p>
+    <p id="userEmail" class="card-text"><span class="iconify" data-icon="carbon:email" style="color: lightgray;" data-width="1" data-height="1"></span> ${users.email}</p>
+    <p id="userWebsite" class="card-text"><span class="iconify" data-icon="akar-icons:link-chain" style="color: lightgray;" data-width="1" data-height="1"></span> ${users.website}</p>
+    <p id="userTwitter" class="card-text"><span class="iconify" data-icon="icon-park-solid:twitter" style="color: lightgray;" data-width="1" data-height="1"></span> Twitter handle</p>
   </div>
   <div>
     <h4>Highlights</h4>
@@ -30,6 +31,7 @@ const sidebarBody = () => {
   </div>
   <div>
     <h4>Sponsors</h4>
+    <p>${users.sponsors}</p>
   </div>
 </div>`
 renderToDom("#sidebarContainer", domString);
