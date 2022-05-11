1a08c256c096abcfdf28aba09a0598f8bb7a2089
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-05-10 21:02:35 -0500

    added button to open modal and added modal framework

--- Components/pin-repos.js
+++ Components/pin-repos.js
@@ -8,12 +8,27 @@ const pinRepos = () => {
     user.repos.forEach((repo) => {
       domString += 
       `
-      <form id="pinnedRepoForm">
-        <li class="repoList">
-          <input type="checkbox" name="repoName" value="Repo">
-          <label for="repoName">${repo.title}</label><br>
-        </li>
-      </form>
+      <div id="pinRepoModal" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Edit Pinned Items</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form id="pinnedRepoForm">
+              <li class="repoList">
+                <input type="checkbox" name="repoName" value="Repo">
+                <label for="repoName">${repo.title}</label><br>
+              </li>
+            </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
       `
       renderToDom("#pinNewRepos", domString);
      })
