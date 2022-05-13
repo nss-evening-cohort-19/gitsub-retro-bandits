0d0a343acfe58d001184c79a86dd9bab078dcb1a
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-05-12 22:00:50 -0500

    trying to get star toggle button to work

--- Components/repolist.js
+++ Components/repolist.js
@@ -1,17 +1,58 @@
 import users from "../Data/data.js"
 import renderToDom from "../Utils/renderToDom.js";
 
-
-const repolist = () => {
+const reposArray = users[0].repos
+export const repolist = () => {
   let domString = '';
-  for (const item of users[0].repos) {
-    domString += `
+  for (const item of reposArray) {
+    if (item.favorite === false) {
+
+      domString += `
+        <div>
+        <a href ="${item.repoUrl}" target="_blank">${item.title}</a>
+        <button type="button" id="starBtn--${item.id}" class="btn btn-light">☆ Star</button>
+        </div>
+      `
+      renderToDom("#repo-container", domString)
+    } else {
+      domString += `
       <div>
-      <h3>${item.title}</h3>
+      <a href ="${item.repoUrl}" target="_blank">${item.title}</a>
+      <button type="button" id="starredBtn--${item.id}" class="btn btn-light">Starred</button>
       </div>
     `
     renderToDom("#repo-container", domString)
+    }
   }
 }
 
-export default repolist
+export const toggleFavorite = (object, index) => {
+  if (object.favorite === false) {
+    const newObject = {
+      id: object.id,
+      title: object.title,
+      repoUrl: object.repoUrl,
+      favorite: true
+    }
+
+    return reposArray.splice(index, 1, newObject)
+  } else {
+    const newObject = {
+      id: object.id,
+      title: object.title,
+      repoUrl: object.repoUrl,
+      favorite: false
+    }
+
+    return reposArray.splice(index, 1, newObject)
+  }
+}
+
+export const starred = document.querySelector("#repo-container")
+starred.addEventListener('click', (e) => {
+  const [method, id] = e.target.id.split("--")
+  const index = reposArray.findIndex((repoIndex => repoIndex.id === Number(id)))
+  
+  toggleFavorite(reposArray[index], index)
+  repolist()
+})
--- Scripts/repos.js
+++ Scripts/repos.js
@@ -2,8 +2,14 @@ import repolist from "../Components/repolist.js";
 import sidebarBody from "../Components/sidebar.js";
 import navBar from "../Components/navbar.js";
 import siteFooter from "../Components/footer.js";
+import starButton from "../Components/repolist.js";
+import toggleFavorite from "../Components/repolist.js";
+import starred from "../Components/repolist.js";
 
 sidebarBody();
 navBar();
 siteFooter();
 repolist();
+starButton();
+toggleFavorite();
+starred()
