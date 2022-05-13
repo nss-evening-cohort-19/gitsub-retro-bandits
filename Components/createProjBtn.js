c5068cc00ec094011a42cec56e8d9a30d605b5c8
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-12 20:04:03 -0500

    add stuff

--- Components/createProjBtn.js
+++ Components/createProjBtn.js
@@ -2,22 +2,23 @@ import users from "../Data/data.js";
 import projectsOnDom from "./projectsOnDom.js";
 
 const createProjBtn = () => {
-  const form = document.querySelector('form');
+  const form = document.querySelector('#new-project-form');
   form.addEventListener('submit', (e) => {
 
     e.preventDefault();
     const newProject = {
       projectName: document.querySelector('#projectName').value,
-      projectUrl: document.querySelector('#description')
+      projectUrl: document.querySelector('#description').value
     }
 
-    users.projects.push(newProject)
-    projectsOnDom()
-    console.log(users)
-
+    users[0].projects.push(newProject);
+    projectsOnDom();
+    console.log(users);  
+    
   
   })
 
+  
 
 
 }
