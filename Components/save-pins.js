2409a0291d931b92a4c594a4dd5d876e0839bbe0
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-05-14 12:42:52 -0500

    got pins on dom-working with data

--- Components/save-pins.js
+++ Components/save-pins.js
@@ -1,43 +1,24 @@
-import renderToDom from "../Utils/renderToDom.js"
+// import renderToDom from "../Utils/renderToDom.js"
 import pinnedRepoArray from "../Data/pinnedRepo.js"
-import data from "../Data/data.js" 
+import users from "../Data/data.js" 
+import pinnedReposOnDom from "./repos-on-dom.js";
 
 const savePins = (event) => {
   console.log(event.target.id);
   if (event.target.id.includes("-input")) {
     const [id] = event.target.id.split("-")
-    const index = data.findIndex(datas => datas.id === parseInt(id));  
+    const index = users.findIndex(data => data.id === parseInt(id));  
     if (pinnedRepoArray.includes(index)){
       pinnedRepoArray.splice(index);
     } else {
-    pinnedRepoArray.push(index);
+      pinnedRepoArray.push(index);
     }
     // check if item is inside pinnedRepoArray
     //  if yes => remove (splice) 
     // if no => add to pinnedRepoArray
   }
+  pinnedReposOnDom();
 }
-
-const reposOnDom = (repo) => {
-  let repoContent = ""
-  pinnedRepoArray.forEach((repo) => {
-    repo.forEach((repository) => {
-      repoContent += `
-        <div>
-          <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <a href="${repository.repoUrl}" class="card-link">${repository.name}</a>
-            </div>
-          </div>
-        </div>
-      `
-    })
-  });
-  
-  renderToDom("#pinnedRepos", repoContent);
-};
-  
-
 
 
 export default savePins
