d416525919fa25bccbe7154b516b310694d7728d
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-14 10:14:49 -0500

    delete something

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -1,7 +1,6 @@
 import sidebarBody from "../Components/sidebar.js";
 import navBar from "../Components/navbar.js";
 import siteFooter from "../Components/footer.js";
-
 import projectsOnDom from "../Components/projectsOnDom.js"
 import createNewProject from "../Components/createNewProject.js";
 import createProjBtn from "../Components/createProjBtn.js";
