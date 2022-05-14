457eaad374f9925fcea518cc42a42a5de2d040a7
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-14 12:33:35 -0500

    added delete button

---
+++ Components/projectDelete.js
@@ -0,0 +1,20 @@
+import users from "../Data/data.js";
+import projectsOnDom from "./projectsOnDom.js";
+
+const deleteProject = () => {
+  document.querySelector('.card').addEventListener('click', (e) => {
+    if (e.target.id) {
+      const [method, projectName] = e.target.id.split('--')
+
+      const index = users[0].projects.findIndex(object => object.projectName === projectName)
+
+      if (e.target.id.includes('delete')) {
+        users[0].projects.splice(index, 1)
+        projectsOnDom()
+        console.log(users[0].projects)
+      }
+    }
+  })
+};
+
+export default deleteProject;
