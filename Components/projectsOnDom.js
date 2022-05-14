7d869627f7a1f39736a341906da344a97ea72e28
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-14 11:08:43 -0500

    refactor project card

--- Components/projectsOnDom.js
+++ Components/projectsOnDom.js
@@ -5,7 +5,7 @@ import renderToDom from "../Utils/renderToDom.js";
 const projectsOnDom = () => {
   let domString = ''
   users[0].projects.forEach(project => {
-    domString += `<a href="#" class="list-group-item list-group-item-action"><h3>${project.projectName}</h3><p>${project.projectUrl}</p> </a>` 
+    domString += `<a href="#" class="list-group-item list-group-item-action"><h3>${project.projectName}</h3><a href="${project.projectUrl}">${project.projectUrl}</a> </a>` 
     
   });
   renderToDom("#list-container", domString)
