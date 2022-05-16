fd5bc8ec90808d45d2183767c6bafb18b8856a78
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-05-16 14:04:11 -0500

    changed icon spans

--- Components/navbar.js
+++ Components/navbar.js
@@ -3,13 +3,13 @@ import renderToDom from "../Utils/renderToDom.js"
 const navBar = () => {
   const domString = `<nav class="navbar navi-btn">
   <form class="container-fluid justify-content-start">
-  <span class="iconify" data-icon="akar-icons:book"></span>
+  <span class="iconify-inline" data-icon="akar-icons:book"></span>
   <a href="../index.html" id="Overview" class="btn navi-btn" type="button">Overview</a>
-  <span class="iconify" data-icon="octicon:repo-24"></span>
+  <span class="iconify-inline" data-icon="octicon:repo-24"></span>
   <a href="../repos.html" id="Repos" class="btn navi-btn" type="button">Repositories</a>
-  <span class="iconify" data-icon="octicon:table-24"></span>
+  <span class="iconify-inline" data-icon="octicon:table-24"></span>
   <a href="../projects.html" id="projects" class="btn navi-btn" type="button">Projects</a>
-  <span class="iconify" data-icon="octicon:package-24"></span>
+  <span class="iconify-inline" data-icon="octicon:package-24"></span>
   <a href="../package.html" id="Packages" class="btn navi-btn" type="button">Packages</a>
     
   </form>
